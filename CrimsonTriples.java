669e0c337dc170c3940377809064f314b94556eb
Shubham Narayan <shubhamnarayan11144@gmail.com>
2026-07-27 23:14:30 +0530

    Crimson

--- CrimsonTriples.java
+++ CrimsonTriples.java
@@ -1,6 +1,10 @@
+import java.io.*;
+
 public class CrimsonTriples {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int t = Integer.parseInt(br.readLine());
         StringBuilder out = new StringBuilder();
 
